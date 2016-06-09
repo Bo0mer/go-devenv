1c895d45052fbca7bf79397ef0f43b673e299583
Ivan Borshukov <bo0merzzz@gmail.com>
2016-06-09 18:10:16 +0300

    Initial commit

---
+++ config-setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+cat > /home/vagrant/.vim_go_runtime/custom_config.vim <<\EOF
+nmap <leader>pj :%!python -m json.tool<CR>
+set autochdir
+
+
+filetype plugin indent on
+autocmd FileType yaml setl indentkeys-=<:>
+autocmd FileType yaml setl tabstop=4 softtabstop=0 expandtab shiftwidth=2 smarttab
+
+
+nmap <leader>gg :!ginkgo<CR>
+nmap <leader>gr :!ginkgo -r<CR>
+
+set relativenumber
+
+function! s:tab_complete()
+    if pumvisible()
+        return "\<C-n>"
+    else
+        " Check whether it's an empty line
+        let col = col('.') - 1
+        if !col || getline('.')[col - 1] =~ '\s'
+            return "\<TAB>"
+        endif
+
+        if neosnippet#expandable_or_jumpable()
+            return "\<Plug>(neosnippet_expand_or_jump)"
+        endif
+
+        return "\<TAB>"
+    endif
+endfunction
+
+imap <silent><expr> <TAB> <SID>tab_complete()
+
+:ab :W :write
+:ab ife if err != nil {<CR>
+:ab ifer if err != nil {<CR>return err}
+:ab td // TODO(ivan):
+
+:syntax off
+
+EOF
---
+++ os-setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e -x
+
+apt-get -y update
+apt-get -y install vim-nox nano curl git wget unzip
+
+wget -qO- https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz | tar -C /usr/local -xzf -
+
+#Set up $GOPATH and add go executables to $PATH
+cat > /etc/profile.d/go_env.sh <<\EOF
+export GOPATH=/home/vagrant/go
+export PATH=$GOPATH/bin:/usr/local/go/bin:$PATH
+EOF
+chmod +x /etc/profile.d/go_env.sh
+
+git clone https://github.com/Bo0mer/vim-go-ide.git /home/vagrant/.vim_go_runtime
+sh /home/vagrant/.vim_go_runtime/bin/install
+
+echo 'set runtimepath^=~/.vim_go_runtime
+source ~/.vim_go_runtime/vimrc/basic.vim
+source ~/.vim_go_runtime/vimrc/filetypes.vim
+source ~/.vim_go_runtime/vimrc/plugins.vim
+source ~/.vim_go_runtime/vimrc/extended.vim
+try
+source ~/.vim_go_runtime/custom_config.vim
+catch
+endtry' > /home/vagrant/.vimrc.go
+
+cat > /etc/profile.d/go_vim.sh <<\EOF
+alias govim='vim -u /home/vagrant/.vimrc.go'
+EOF
+chmod +x /etc/profile.d/go_vim.sh
+
